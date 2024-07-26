5dfbb16a6138010142ba034b90b4befc8e478e79
egorusegorus <tomaszokraszewski@gmail.com>
2024-07-26 08:33:51 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lotery2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lotery(20, 0, 99);
+            Console.ReadKey();
+        }
+        public static void Lotery(int wieVielZahlen, int bereichBegin, int bereichEnde)
+        {
+            string? b = "";
+            for (int i = 0; i < wieVielZahlen; i++)
+            {
+                Random r = new Random();
+                int c = r.Next(bereichBegin, bereichEnde);
+                Console.WriteLine("Zahl " + (i + 1) + "=" + c);
+            }
+
+        }
+    }
+}
